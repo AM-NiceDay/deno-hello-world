737a47fe1e1ede022f38bf56c9ed7177c0740a02
Aliaksei Krasikau <a.krasikau@gmail.com>
2020-05-14 08:07:38 +0300

    Hello Deno

---
+++ index.ts
@@ -0,0 +1,9 @@
+import { serve } from "https://deno.land/std@0.50.0/http/server.ts";
+
+const s = serve({ port: 8080 });
+
+console.log("Deno is listening on http://localhost:8080/");
+
+for await (const req of s) {
+  req.respond({ body: "Hello World\n" });
+}
